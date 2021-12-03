35c9168d66f289bb5cfa0dfe55c3c968b85650ec
sylvain <sguerrin549@gmail.com>
2021-12-03 18:03:10 +0100

    add house component

---
+++ houses.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        picture: '/images/1.jpg',
+        type: 'Entire house',
+        town: 'New York',
+        title: 'Beautiful flat in NY !'
+    },
+    {
+        picture: '/images/2.jpg',
+        type: 'Entire house',
+        town: 'Ireland',
+        title: 'Beautiful house in Ireland !'
+    }
+]
